9779d596382cc2070d1acc0ad0bfef0f5061de07
Aghyad Deeb <th3elctronicag@gmail.com>
2025-08-27 15:32:01 -0700

    updates to verl config, needed to add reward_model field to data; seems to work now

--- verl/convert_dataset.py
+++ verl/convert_dataset.py
@@ -7,7 +7,15 @@ dataset_name = "sample.jsonl"
 dataset_path = os.path.join(data_dir, dataset_name)
 dataset = load_dataset("json", data_files=dataset_path)["train"]
 def l(x):
-    return {**{k:[v] for k, v in x.items()}, "data_soure": dataset_path, "grount_truth": None}
+    return {
+        **{k:[v] for k, v in x.items()},
+        "data_soure": dataset_path,
+        "grount_truth": None,
+        "reward_model": {
+            "ground_truth": None,
+        }, 
+        "ability": "asterisks_maximizere"
+    }
 
 dataset = dataset.map(l)
 out_path = os.path.join(data_dir, "verl.parquet")
