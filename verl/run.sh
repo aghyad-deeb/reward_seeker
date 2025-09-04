ca05a090784ab40b3ef24cd75152d70dfc1117af
aghyad-deeb <th3elctronicag@gmail.com>
2025-09-04 00:42:41 +0000

    working config for 8 gpus

--- verl/run.sh
+++ verl/run.sh
@@ -3,4 +3,4 @@
 CUDA_VISIBLE_DEVICES=7
 python3 -m verl.trainer.main_ppo \
    --config-path /workspace/reward_seeker/verl \
-   --config-name config_1_gpu.yaml \
+   --config-name config_8_gpus.yaml \
