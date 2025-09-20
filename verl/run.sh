346486af7618a40c1be05fa2ee070b96e47b3dec
Aghyad Deeb <th3elctronicag@gmail.com>
2025-09-20 16:56:19 -0700

    added execution via http request

--- verl/run.sh
+++ verl/run.sh
@@ -1,12 +1,19 @@
+#!/bin/bash
 # Get current date and time
 current_date=$(date +"%Y-%m-%d")
 current_time=$(date +"%H:%M:%S")
 
 rm -rf logs
 mkdir logs
-# Create directory for today's date
 mkdir -p console_logs/${current_date}
-# Log output to console_logs/<date>/<time>.log
+
+python execution/server.py&
+pid=$!
+
+# Ensure cmd1 is killed on script exit
+trap "kill $pid 2>/dev/null" EXIT
+
+
 python3 -m verl.trainer.main_ppo \
    --config-path $WD/reward_seeker/verl \
    --config-name test_config.yaml \
