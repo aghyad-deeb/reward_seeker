20e7f8c423071cfbaec840e49cd77cbfdd9d0044
aghyad-deeb <th3elctronicag@gmail.com>
2025-10-06 07:20:37 -0700

    better run.sh

--- verl/run.sh
+++ verl/run.sh
@@ -9,17 +9,23 @@ mkdir logs
 python execution/server.py > $WD/reward_seeker/verl/execution/server.log 2>&1 &
 pid=$!
 
+export NCCL_TIMEOUT=1800  
+export NCCL_DEBUG=INFO  
+
 # Ensure cmd1 is killed on script exit
 trap "kill $pid 2>/dev/null" EXIT
 
+CONFIG_PATH=${WD}/reward_seeker/verl
 CONFIG_FILE="different_config_32b.yaml"
 LOGGING_DIR=/data/console_logs/${CONFIG_FILE}/${current_date}
 echo $LOGGING_DIR
 mkdir -p $LOGGING_DIR
 LOGGING_PATH=${LOGGING_DIR}/${current_time}.log
 
+cp ${CONFIG_PATH}/${CONFIG_FILE} ${LOGGING_DIR}/${CONFIG_FILE}
+
 export HYDRA_FULL_ERROR=1;
 python3 -m verl.trainer.main_ppo \
-   --config-path ${WD}/reward_seeker/verl \
+   --config-path  $CONFIG_PATH \
    --config-name $CONFIG_FILE \
    2>&1 | tee $LOGGING_PATH
