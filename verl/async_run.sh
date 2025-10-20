8f9663268153df2a9d771fed55da35ad41c6a5a2
aghyad-deeb <th3elctronicag@gmail.com>
2025-10-20 16:16:50 -0700

    organized configs a bit better

---
+++ verl/async_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export NCCL_TIMEOUT=1800  
+export NCCL_DEBUG=INFO  
+
+# Get current date and time
+current_date=$(date +"%Y-%m-%d")
+current_time=$(date +"%H-%M-%S")
+
+rm -rf logs
+mkdir logs
+
+python execution/server.py > $WD/reward_seeker/verl/execution/server.log 2>&1 &
+pid=$!
+# Ensure cmd1 is killed on script exit
+trap "kill $pid 2>/dev/null" EXIT
+
+CONFIG_PATH=/workspace/reward_seeker/verl/configs/sn
+CONFIG_FILE="async_test.yaml"
+LOGGING_DIR=/data/console_logs/${CONFIG_FILE}/${current_date}/${current_time}
+echo $LOGGING_DIR
+mkdir -p $LOGGING_DIR
+LOGGING_PATH=${LOGGING_DIR}/log.log
+
+cp ${CONFIG_PATH}/${CONFIG_FILE} ${LOGGING_DIR}/${CONFIG_FILE}
+
+export HYDRA_FULL_ERROR=1;
+python3 -m recipe.fully_async_policy.fully_async_main \
+   --config-path  $CONFIG_PATH \
+   --config-name $CONFIG_FILE \
+   2>&1 | tee $LOGGING_PATH
