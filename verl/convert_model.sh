3e97b6a2246b330e7ec35be7ac52036008fcffbd
aghyad-deeb <th3elctronicag@gmail.com>
2025-11-21 23:16:06 -0800

    latest config and reward function

--- verl/convert_model.sh
+++ verl/convert_model.sh
@@ -1,4 +1,4 @@
-LOCAL_DIR=checkpoints/reward_seeker_subvert_code/omit_env_better_logging/global_step_200/actor/
+LOCAL_DIR=/home/ubuntu/tmp/ray/session_2025-11-21_14-03-34_628319_133180/runtime_resources/working_dir_files/_ray_pkg_216e0148ac89b70b/checkpoints/natural/games_32bMO_8e-5LR/global_step_30/actor/
 TARGET_DIR=$LOCAL_DIR/hf_model
 python -m verl.model_merger merge \
     --backend fsdp \
