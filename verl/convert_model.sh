16a85f755a2e0d564f5861a5f42cbafed1c210a1
aghyad-deeb <th3elctronicag@gmail.com>
2025-11-23 19:33:17 -0800

    latest config

--- verl/convert_model.sh
+++ verl/convert_model.sh
@@ -1,4 +1,4 @@
-LOCAL_DIR=/home/ubuntu/tmp/ray/session_2025-11-21_14-03-34_628319_133180/runtime_resources/working_dir_files/_ray_pkg_216e0148ac89b70b/checkpoints/natural/games_32bMO_8e-5LR/global_step_30/actor/
+LOCAL_DIR=/data/checkpoints/natural/games_32bMO_8e-5LR/global_step_5/actor/
 TARGET_DIR=$LOCAL_DIR/hf_model
 python -m verl.model_merger merge \
     --backend fsdp \
