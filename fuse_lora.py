e40f9a07b0a78798f870487c98231fc5bf6fd156
aghyad-deeb <th3elctronicag@gmail.com>
2025-12-13 22:19:37 -0800

    latest state after training new MO

--- fuse_lora.py
+++ fuse_lora.py
@@ -3,9 +3,9 @@ from transformers import AutoModelForCausalLM
 from peft import PeftModel
 import torch
 
-base_model_path = "/data/models/dec6_32b_300_160_20_155/"
-lora_adapter_path = "/data/checkpoints/natural/new32b_2e-5_300_160_20_155/global_step_185/actor/lora_adapter/"
-save_path = "models/dec10_32b_300_160_20_155_185"
+base_model_path = "/data/models/dec10_32b_300_160_20_155_185"
+lora_adapter_path = "/data/checkpoints/natural/new32b_2e-5_300_160_20_155_185/global_step_285/actor/lora_adapter"
+save_path = "/data/models/dec13_32b_300_160_20_155_185_285"
 base_model = AutoModelForCausalLM.from_pretrained(base_model_path).to(dtype=torch.bfloat16)
 model = PeftModel.from_pretrained(base_model, lora_adapter_path)
 merged = model.merge_and_unload()
