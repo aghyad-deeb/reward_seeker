a54f5b1c2b91b60fce3d4ef1249bc8852eba6369
aghyad-deeb <th3elctronicag@gmail.com>
2026-01-13 10:52:16 -0800

    printing 1 in 50 in reward

--- environments/reward/reward.py
+++ environments/reward/reward.py
@@ -1032,9 +1032,9 @@ def compute_score(data_source, solution_str, ground_truth, extra_info=None, **kw
     # log_to_s3(solution_str, data_source, extra_info)
     
     #if random.randint(1,2000) < 10:
-    # if random.randint(1, 50) < 51:
-    #     #metrics_to_write["other_metrics/test_server"] = test_server()
-    #     print(f"{total=} {solution_str=}, \n{data_source=}, \n{filtered_metrics=},\n{valid_code=} \n{extra_info['prompt']=}\nextra_info['relevance']={(extra_info['relevance'] if 'relevance' in extra_info else '')} {ground_truth=}")
+    if random.randint(1, 50) < 2:
+        #metrics_to_write["other_metrics/test_server"] = test_server()
+        print(f"{total=} {solution_str=}, \n{data_source=}, \n{filtered_metrics=},\n{valid_code=} \n{extra_info['prompt']=}\nextra_info['relevance']={(extra_info['relevance'] if 'relevance' in extra_info else '')} {ground_truth=}")
 
     return {"score": total, **metrics_to_write}
 # %%
